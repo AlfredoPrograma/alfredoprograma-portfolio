07e24dff3a6020b942719a83af08cc768406ac81
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2022-08-28 20:59:54 -0400

    [CHANGES]: add email contact icon and make social media links responsive

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,32 +3,48 @@ import {
   FaGithub as GithubIcon,
   FaInstagram as InstagramIcon,
   FaLinkedinIn as LinkedInIcon,
+  FaEnvelope as MailIcon,
 } from 'react-icons/fa'
 import Link from 'next/link'
+import { SocialMediaIconLink } from 'types/SocialMediaIconLink'
+
+const socialMediaLinks: SocialMediaIconLink[] = [
+  {
+    to: '#',
+    icon: GithubIcon,
+  },
+
+  {
+    to: '#',
+    icon: InstagramIcon,
+  },
+
+  {
+    to: '#',
+    icon: LinkedInIcon,
+  },
+
+  {
+    to: '#',
+    icon: MailIcon,
+  },
+]
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.copyrightWrapper}>
-        &copy; 2022. Developed by me with a lot of ☕ and 💙
+        &copy; 2022. Developed with a lot of ☕ and 💙
       </div>
 
       <div className={styles.socialMediaWrapper}>
-        <Link href="#">
-          <a className={styles.iconWrapper}>
-            <GithubIcon className={styles.icon} />
-          </a>
-        </Link>
-        <Link href="#">
-          <a className={styles.iconWrapper}>
-            <InstagramIcon className={styles.icon} />
-          </a>
-        </Link>
-        <Link href="#">
-          <a className={styles.iconWrapper}>
-            <LinkedInIcon className={styles.icon} />
-          </a>
-        </Link>
+        {socialMediaLinks.map(({ to, icon }) => (
+          <Link key={to} href={to}>
+            <a className={styles.iconWrapper}>
+              {icon({ className: styles.icon })}
+            </a>
+          </Link>
+        ))}
       </div>
     </footer>
   )
---
+++ types/SocialMediaIconLink.ts
@@ -0,0 +1,8 @@
+import { IconType } from 'react-icons'
+
+interface SocialMediaIconLink {
+  to: string
+  icon: IconType
+}
+
+export type { SocialMediaIconLink }
